3e8b28a85bed0cc7c247cdeda699ad6f02136a41
IsaqjonovII <ilhomjonisoqjonov2@gmail.com>
2024-04-25 00:11:50 +0500

    batman

---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+
+<template>
+    <div class="container flex navbar">
+       <div class="lang__dropdown">
+            
+       </div>
+    </div>
+</template>
+
+
+<style scoped>
+    .navbar {
+        background-color: #fff;
+        height: 80px;
+        padding: 0 50px;
+    }
+</style>
