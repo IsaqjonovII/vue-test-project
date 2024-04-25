ec4d4b2360ac7fc56794276903e7da0bfba3e85c
IsaqjonovII <ilhomjonisoqjonov2@gmail.com>
2024-04-25 16:53:06 +0500

    carousel is nearly done

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -5,32 +5,79 @@ import galleryData from "../mocks";
 
 const currentImgIndex = ref(0);
 
-console.log(currentImgIndex.value);
+function nextImage() {
+  if (galleryData.length - 1 === currentImgIndex.value) {
+    currentImgIndex.value = 0;
+  } else {
+    currentImgIndex.value++;
+  }
+}
+function prevImage() {
+  if (currentImgIndex.value === 0) {
+    currentImgIndex.value = galleryData.length - 1;
+  } else {
+    currentImgIndex.value--;
+  }
+}
+
+const currentImg = computed(() => galleryData[currentImgIndex.value].img);
+
+function handleImgClick(id) {
+  currentImgIndex.value = id;
+}
 </script>
 
 <template>
-  <div class="max-w-[835px] p-4 bg-white rounded-xl" >
+  <div class="max-w-[835px] p-4 bg-white rounded-xl">
     <div class="bg-red-500 rounded-xl relative">
       <button
+        @click="prevImage"
         class="p-3 bg-gray-200 rounded-full hover:bg-blue-600 absolute top-1/2 -left-3 hover:text-white transition-colors"
       >
         <ChevronLeftIcon class="h-5 w-5 align-middle" />
       </button>
 
-      <div class="w-full max-w-[834px] bg-red-400">
-        <img :src="galleryData[currentImgIndex.value ?? 0].img" alt="" />
+      <div
+        v-if="galleryData.length > 0"
+        class="w-full max-w-[834px] h-[500px] bg-red-400"
+      >
+        <img
+          :src="currentImg"
+          alt=""
+          class="w-full h-full object-cover object-center"
+        />
       </div>
 
       <button
+        @click="nextImage"
         class="p-3 bg-gray-200 rounded-full hover:bg-blue-600 absolute top-1/2 -right-3 hover:text-white transition-colors"
       >
         <ChevronRightIcon class="h-5 w-5 align-middle" />
       </button>
     </div>
-    <div class="flex items-center gap-3 overflow-x-hidden">
-      <div v-for="item in galleryData" :key="item.id" class="w-full max-w-28">
-        <img class="w-full h-28 object-cover object-center" :src="item.img" alt="gallery img">
-      </div>
+    <div
+      class="w-full h-28 overflow-x-auto overflow-y-hidden flex items-center gap-2 py-2 relative no-scrollbar"
+    >
+      <button
+        @click="prevImage"
+        class="p-3 bg-gray-200 rounded-full hover:bg-blue-600 absolute top-[calc(1/2 - 35px)] left-3 hover:text-white transition-colors"
+      >
+        <ChevronLeftIcon class="h-5 w-5 align-middle" />
+      </button>
+      <img
+        v-for="item in galleryData"
+        :key="item.id"
+        class="w-32 h-full object-cover object-center rounded-md cursor-pointer"
+        :src="item.img"
+        @click="() => handleImgClick(item.id)"
+        alt="gallery img"
+      />
+      <button
+        @click="nextImage"
+        class="p-3 bg-gray-200 rounded-full hover:bg-blue-600 absolute top-[calc(1/2 - 35px) right-3 hover:text-white transition-colors"
+      >
+        <ChevronRightIcon class="h-5 w-5 align-middle" />
+      </button>
     </div>
   </div>
 </template>
--- src/mocks/index.js
+++ src/mocks/index.js
@@ -1,8 +1,8 @@
 const galleryData = [];
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 15; i++) {
     galleryData.push({
         id: i,
-        img: "https://cdn.dealerspike.com/imglib/v1/800x600/imglib/Assets/Inventory/FD/97/FD977C48-5985-44E1-90AF-CA07C303BE00.jpg",
+        img: i % 2 === 0 ? "https://cdn.dealerspike.com/imglib/v1/800x600/imglib/Assets/Inventory/FD/97/FD977C48-5985-44E1-90AF-CA07C303BE00.jpg" : "https://www.webike.es/magazine/wp-content/uploads/2022/10/KAWASAKI_Z1000_01.jpg",
     });
 }
 
